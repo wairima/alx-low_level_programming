62e6b3f222935c59379c3ea3827b1fe942b64cb0
wairima <chrwairima@gmailcom>
2023-08-11 22:44:29 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+  * main - Entry point
+  * Return: always 0 (success)
+  */
+int main(void)
+{
+	int i;
+
+	char c;
+
+	i = 0;
+	c = 'a';
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		i++;
+	}
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
