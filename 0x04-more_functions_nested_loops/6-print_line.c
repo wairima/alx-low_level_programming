d3448ce0609861a39c934cf57d4497d4bac91e3c
wairima <chrwairima@gmailcom>
2023-08-17 01:59:42 -0700

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * print_line - straight linee
+  * @n: number of times
+  * Return: always 0
+  */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
