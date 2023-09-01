eb87d0a7753c587ac9127cd9e22aef8c45f49c1a
wairima <chrwairima@gmail.com>
2023-09-01 05:04:20 -0700

    Create the static library libmy.a containing all the functions

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
  * _memset - fill a block of memory with a specific value
- * @a: starting address of memory to be filled
+ * @s: starting address of memory to be filled
  * @b: the desired value
  * @n: number of bytes to be changed
  *
  * Return: changed array with new value for n bytes
  */
-char *_memset(char *a, char d, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	int e = 0;
+	int i = 0;
 
-	for (; n > 0; e++)
+	for (; n > 0; i++)
 	{
-		a[e] = d;
+		s[i] = b;
 		n--;
 	}
-	return (a);
+	return (s);
 }
